78a2e41bdceb49308c0cecc22219d8915ca54b6c
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2023-12-10 20:28:14 +0300

    database info replaced with env file vars

--- app/database.py
+++ app/database.py
@@ -1,13 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import URL
 from sqlmodel import SQLModel, create_engine, Session
 
+load_dotenv()
 
 url_object = URL.create(
-    "postgresql",
-    username="postgres",
-    password="mmz18102002zmm",
-    host="localhost",
-    database="banking_system",
+    os.getenv('DATABASE'),
+    username=os.getenv('POSTGRES_USER'),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv('POSTGRES_HOST'),
+    database=os.getenv('POSTGRES_DB'),
 )
 engine = create_engine(url_object)
 
